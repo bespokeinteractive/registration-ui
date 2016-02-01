082797b4e4624b1497c679258aa29c6f1a2a842a
stansL <@dmin2@14!>
2016-02-01 18:19:21 +0300

    listing of return patients and relevant actions

--- omod/src/main/java/org/openmrs/module/registration/page/controller/RevisitPatientRegistrationPageController.java
+++ omod/src/main/java/org/openmrs/module/registration/page/controller/RevisitPatientRegistrationPageController.java
@@ -4,15 +4,16 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dom4j.DocumentException;
 import org.jaxen.JaxenException;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.hospitalcore.HospitalCoreService;
 import org.openmrs.module.registration.util.RegistrationConstants;
 import org.openmrs.module.registration.util.RegistrationUtils;
 import org.openmrs.module.registration.web.controller.util.RegistrationWebUtils;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.openmrs.ui.framework.page.PageModel;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author Stanslaus Odhiambo
@@ -21,9 +22,11 @@ import java.io.IOException;
 public class RevisitPatientRegistrationPageController {
     private static Log logger = LogFactory.getLog(RevisitPatientRegistrationPageController.class);
 
-    public void get(HttpServletRequest request, Model model)
+    public void get(HttpServletRequest request, PageModel model)
             throws JaxenException, DocumentException, IOException {
-        model.addAttribute("patientIdentifier", RegistrationUtils.getNewIdentifier());
+        HospitalCoreService hospitalCoreService = (HospitalCoreService) Context.getService(HospitalCoreService.class);
+        model.addAttribute("patientIdentifier",
+                RegistrationUtils.getNewIdentifier());
         model.addAttribute("referralHospitals", RegistrationWebUtils
                         .getSubConcepts(RegistrationConstants.CONCEPT_NAME_PATIENT_REFERRED_FROM));
         model.addAttribute("referralReasons", RegistrationWebUtils
@@ -38,6 +41,8 @@ public class RevisitPatientRegistrationPageController {
                 .getSubConcepts(RegistrationConstants.CONCEPT_NAME_TRIAGE));
         model.addAttribute("OPDs", RegistrationWebUtils
                         .getSubConcepts(RegistrationConstants.CONCEPT_NAME_OPD_WARD));
+        model.addAttribute("location", Context.getAdministrationService().getGlobalProperty("hospitalcore.hospitalName"));
+        model.addAttribute("listOfPatients", new ArrayList<String>());
 
     }
 
--- omod/src/main/webapp/pages/revisitPatientRegistration.gsp
+++ omod/src/main/webapp/pages/revisitPatientRegistration.gsp
@@ -1,3 +1,50 @@
-<% ui.decorateWith("appui", "standardEmrPage", [title: "Patient Registration"]) %>
+<% ui.decorateWith("appui", "standardEmrPage", [title: "Patient Revisit"]) %>
 
-<h3 align="center">Under Dev</h3>
+<body></body>
+<header>
+</header>
+<div class="clear"></div>
+<div class="container">
+    <div class="example">
+        <ul id="breadcrumbs">
+            <li>
+                <a href="#">
+                    <i class="icon-home small"></i></a>
+            </li>
+            <li>
+                <i class="icon-chevron-right link"></i>
+                <a href="#">Patient Revisit</a>
+            </li>
+            <li>
+            </li>
+        </ul>
+    </div>
+    <div class="patient-header new-patient-header">
+        <div class="demographics">
+            <h1 class="name" style="border-bottom: 1px solid #ddd;">
+                <span>PATIENTS REVISIT &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            </h1>
+
+            <br>
+
+
+        </div>
+
+        <div class="identifiers">
+            <em>Current Time:</em>
+            <span>25-01-2016 10:56:41</span>
+        </div>
+
+
+        <div class="onerow">
+            <div style="text-align: center; ">
+                <centre>
+                    ${ ui.includeFragment("registration", "revisitPatientRegistrationPage") }
+                </centre>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</div>
